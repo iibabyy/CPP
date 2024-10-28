3bbefb829bbfc6c96fe258876a45baaa0766e6c4
Idrissa Baby <ibaby@made-f0Ar8s11.clusters.42paris.fr>
2024-10-28 19:12:28 +0100

    ex00 fixed

--- m06/ex00/srcs/ScalarConverter.cpp
+++ m06/ex00/srcs/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: ibaby <ibaby@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 17:30:31 by ibaby             #+#    #+#             */
-/*   Updated: 2024/10/28 18:31:53 by ibaby            ###   ########.fr       */
+/*   Updated: 2024/10/28 19:09:34 by ibaby            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,136 +35,134 @@ ScalarConverter&	ScalarConverter::operator=( const ScalarConverter& other ) {
 	return (*this);
 }
 
+void	impossible( void ) {
+	std::cout << "char: impossible" << std::endl;
+	std::cout << "int: impossible" << std::endl;
+	std::cout << "float: impossible" << std::endl;
+	std::cout << "double: impossible" << std::endl;
+}
+
+void	specialValue( std::string str ) {
+	std::cout << "char: impossible" << std::endl;
+	std::cout << "int: impossible" << std::endl;
+	std::cout << "float: " << str << "f" << std::endl;
+	std::cout << "double: " << str << std::endl;
+}
+
 /* Convert */
-
-// float	strToFloat( std::string str ) {
-// 	float result;
-
-//     result = std::stof(str);
-
-//     return result;
-// }
-
-// int	strToInteger( std::string str ) {
-	
-//     int result = std::stoi(str);
-  
-//     return (result);
-// }
-
-// double	strToDouble( std::string str ) {
-// 	double result;
-
-//     result = std::stod(str);
-
-//     return result;
-// }
 
 void	convertFloat( std::string str) {
 
-	std::cout << "float: ";
+	float	num;
 
-	try {
-		if (str == "nan") {
-			std::cout << "nanf" << std::endl;
-		} else if (str == "inf" || str == "+inf") {
-			std::cout << "+inff" << std::endl;
-		} else if (str == "-inf") {
-			std::cout << "-inff" << std::endl;
-		} else {
-			float num;
-			std::stringstream ss(str);
-			ss >> num;
+	if (str == "nan") {
+		return specialValue("nan");
+	} else if (str == "inf" || str == "+inf") {
+		return specialValue("+inf");
+	} else if (str == "-inf") {
+		return specialValue("-inf");
+	} else {
+		std::stringstream ss(str);
+		ss >> num;
+		if (ss.fail())
+			return impossible();
+	}
 
-			if (ss.fail())
-				throw std::invalid_argument("impossible");
-			
-			if (std::floor(num) == num)
-				std::cout << std::fixed << std::setprecision(1) << num << "f" << std::endl;
-			else
-				std::cout << num << "f" << std::endl;
-
-		}
-	} catch (...) {
-		std::cout << "impossible" << std::endl;
+	if (std::isprint(static_cast<char>(num)) == false)
+		std::cout << "char: Non displayable" << std::endl;
+	else
+		std::cout << "char: " << static_cast<char>(num) << std::endl;
+	std::cout << "int: " << static_cast<int>(num) << std::endl;
+	if (std::floor(num) == num) {
+		std::cout << "float: " << std::fixed << std::setprecision(1) << num << "f" << std::endl;
+		std::cout << "double: "<< std::fixed << std::setprecision(1) << static_cast<double>(num) << std::endl;
+	} else {
+		std::cout << "float: " << num << "f" << std::endl;
+		std::cout << "double: " << static_cast<double>(num) << std::endl;
 	}
 }
 
 void	convertInteger( std::string str ) {
-    int num;
-    std::stringstream ss(str);
-    ss >> num;
 
-	std::cout << "int: ";
-	if (ss.fail()) {
-        std::cout << "impossible" << std::endl;
-    } else {
-		std::cout << num << std::endl;
-	}
+	int	num;
+
+	std::stringstream ss(str);
+	ss >> num;
+	if (ss.fail())
+		return impossible();
+
+	if (std::isprint(static_cast<char>(num)) == false)
+		std::cout << "char: Non displayable" << std::endl;
+	else
+		std::cout << "char: " << static_cast<char>(num) << std::endl;	
+	std::cout << "int: " << num << std::endl;
+	std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(num) << "f" << std::endl;
+	std::cout << "double: "<< std::fixed << std::setprecision(1) << static_cast<double>(num) << std::endl;
 }
 
 void	convertDouble( std::string str ) {
-	std::cout << "double: ";
+	double	num;
+
 	if (str == "nan") {
-		std::cout << "nan" << std::endl;
+		return specialValue("nan");
 	} else if (str == "inf" || str == "+inf") {
-		std::cout << "+inf" << std::endl;
+		return specialValue("+inf");
 	} else if (str == "-inf") {
-		std::cout << "-inf" << std::endl;
+		return specialValue("-inf");
 	} else {
-		double num;
 		std::stringstream ss(str);
 		ss >> num;
-		
-		if (ss.fail()) {
-			std::cout << "impossible" << std::endl;
-			return ;
-		}
-		if (std::floor(num) == num)
-			std::cout << std::fixed << std::setprecision(1) << num << std::endl;
-		else
-			std::cout << num << std::endl;
+		if (ss.fail())
+			return impossible();
+	}
+
+	if (std::isprint(static_cast<char>(num)) == false)
+		std::cout << "char: Non displayable" << std::endl;
+	else
+		std::cout << "char: " << static_cast<char>(num) << std::endl;
+	std::cout << "int: " << static_cast<int>(num) << std::endl;
+	if (std::floor(num) == num) {
+		std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(num) << "f" << std::endl;
+		std::cout << "double: "<< std::fixed << std::setprecision(1) << num << std::endl;
+	} else {
+		std::cout << "float: " << static_cast<float>(num) << "f" << std::endl;
+		std::cout << "double: " << num << std::endl;
 	}
 }
 
-void	convertChar( std::string str ) {
-	std::cout << "char: ";
+// void	convertChar( std::string str ) {
+// 	std::cout << "char: ";
 
-	try {
-		int c;
-		std::stringstream ss(str);
-		ss >> c;
+// 	try {
+// 		int c;
+// 		std::stringstream ss(str);
+// 		ss >> c;
 		
-		if (ss.fail()) {
-			std::cout << "impossible" << std::endl;
-		} else if (c < 0 || c > 256) {
-			std::cout << "impossible" << std::endl;
-		} else if (std::isprint(c) == false) {
-			std::cout << "Non displayable" << std::endl;
-		} else {
-			std::cout << static_cast<char>(c) << std::endl;
-		}
+// 		if (ss.fail()) {
+// 			std::cout << "impossible" << std::endl;
+// 		} else if (c < 0 || c > 256) {
+// 			std::cout << "impossible" << std::endl;
+// 		} else if (std::isprint(c) == false) {
+// 			std::cout << "Non displayable" << std::endl;
+// 		} else {
+// 			std::cout << static_cast<char>(c) << std::endl;
+// 		}
 
-	} catch (...) {
-		std::cout << "impossible" << std::endl;
-	}
-}
+// 	} catch (...) {
+// 		std::cout << "impossible" << std::endl;
+// 	}
+// }
 
 void ScalarConverter::convert( std::string str ) {
 	if (str.empty() == true) {
 		std::cout << "Empty argument not allowed" << std::endl;
 	}
 
-	// if (str.find('f') != std::string::npos) {
-	// 	fromFloat(str);
-	// } else if (str.find('.') != std::string::npos) {
-	// 	fromDouble(str);
-	// } else {
-	// 	fromInt(str);
-	// }
-	
-	convertChar(str);
-	convertInteger(str);
-	convertFloat(str);
-	convertDouble(str);
+	if (str.find('f') != std::string::npos) {
+		convertFloat(str);
+	} else if (str.find('.') != std::string::npos) {
+		convertDouble(str);
+	} else {
+		convertInteger(str);
+	}
 }
